8755ddf4120adf2fe57f8ee7d6d6e04357ce8dfd
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-07-25 12:58:20 +0700

    maintenance page 🛠️

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import Head from 'next/head'
 import Image from 'next/image'
+import logoRancang from '../public/icon/rancang-horizontal.png'
 
 export default function Home() {
   return (
-    <>
-      <p>
-        Rancang Creative
-      </p>
-    </>
+    <div className='bg-primary/70 h-screen w-full flex flex-col justify-center items-center'>
+      <div className='w-40 mb-4'>
+        <Image alt='Logo Rancang' src={logoRancang} />
+      </div>
+      This website is under development
+    </div>
   )
 }
