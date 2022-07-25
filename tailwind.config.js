d4c1128d6326b30d69144b674b6b845b408a729c
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-07-25 12:46:50 +0700

    Add assets 👍

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'daxline' : ['"Daxline Pro"', 'sans-serif'],
+      'poppins' : ['Poppins', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'primary': '#80E4E2',
+        'secondary': '#F1545D',
+      }
+    },
+  },
+  plugins: [],
+}
