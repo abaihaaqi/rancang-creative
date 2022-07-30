e68dbd000331e8835bfc69f84991d83c31609662
Nizar <nizar.albaihaqi252@gmail.com>
2022-07-30 13:42:01 +0700

    Add footer 🛠️

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,5 +1,6 @@
-import Navbar from "../components/navbar"
-import MetaTags from "../utils/meta-tag"
+import Navbar from '../components/navbar'
+import MetaTags from '../utils/meta-tag'
+import { WhatsappLogo } from 'phosphor-react'
 
 export default function Layout({ children, metaContent }) {
   return (
@@ -9,6 +10,14 @@ export default function Layout({ children, metaContent }) {
       <main>
         {children}
       </main>
+      <div className='relative bg-gray-700 text-center py-2 z-50'>
+        <a href='http://wa.me/6285624200502?text=Hi,+mau+dong+sosmednya+dirapihin' target='_blank' rel='noopener noreferrer'>
+          <WhatsappLogo size={32} className='text-green-400 mx-auto' /> 
+        </a>
+        <p className='text-sm mt-2 text-white'>
+          ©️ Rancang Creative - Based in Bandung
+        </p>
+      </div>
     </>
   )
 }
